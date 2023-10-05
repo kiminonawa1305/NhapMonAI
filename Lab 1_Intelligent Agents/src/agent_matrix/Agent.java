ccb5d4b0d2fdf153075e93df7368dbd813cb459e
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-05 17:12:42 +0700

    Thêm comment cho các function của Agent và AgentProgram

--- Lab 1_Intelligent Agents/src/agent_matrix/Agent.java
+++ Lab 1_Intelligent Agents/src/agent_matrix/Agent.java
@@ -11,18 +11,15 @@ public class Agent {
         program = aProgram;
     }
 
+    /**
+     * Trả về hành động khi biết được vị trí hiện tại của con agent và trạng thái của nó
+     * @param p
+     * @return
+     */
     public Action execute(Percept p) {
         if (program != null) {
             Action action = program.execute(p);
-            if (action.equals(Environment.SUCK_DIRT)) {
-                score += 500;
-            } else {
-                if (action.equals(Environment.NO_OP_ACTION)) {
-                    score -= 100;
-                } else {
-                    score -= 10;
-                }
-            }
+            setScore(action);
 
             return program.execute(p);
         }
@@ -30,15 +27,39 @@ public class Agent {
         return NoOpAction.NO_OP;
     }
 
+    /**
+     * Chuyển đổi ma trận thành đồ thị hổ trợ trong việc tính toán đường đi
+     * @param matrixState
+     */
     public void convertMatrixStateToGraph(Environment.LocationState[][] matrixState) {
         program.convertMatrixStateToGraph(matrixState);
     }
 
+    /**
+     * Kiểm tra xem con agent đã vận hành xong chưa xong chưa
+     * @return
+     */
     public boolean isDone() {
         return program.isDone();
     }
 
     public int getScore(){
         return score;
+    }
+
+    /**
+     * Tính điểm dựa vào action của con agent
+     * @param action
+     */
+    private void setScore(Action action){
+        if (action.equals(Environment.SUCK_DIRT)) {
+            score += 500;
+        } else {
+            if (action.equals(Environment.NO_OP_ACTION)) {
+                score -= 100;
+            } else {
+                score -= 10;
+            }
+        }
     }
 }
--- Lab 1_Intelligent Agents/src/agent_matrix/AgentProgram.java
+++ Lab 1_Intelligent Agents/src/agent_matrix/AgentProgram.java
@@ -8,6 +8,10 @@ public class AgentProgram {
     private Graph graph;
     private LinkedList<Location> listLocateIsDirty;
 
+    /**
+     * Chuyển ma trận thành đồ thị
+     * @param matrixState
+     */
     public void convertMatrixStateToGraph(LocationState[][] matrixState) {
         this.graph = new Graph(matrixState);
         scanEnvironment(matrixState);
@@ -26,6 +30,10 @@ public class AgentProgram {
     }
 
 
+    /**
+     * Quét ma trận sẽ thực thi để có thể tìm ra các vị trí đang bị dơ
+     * @param state
+     */
     public void scanEnvironment(Environment.LocationState[][] state) {
         listLocateIsDirty = new LinkedList<Location>();
         for (int row = 0; row < state.length; row++) {
@@ -37,13 +45,27 @@ public class AgentProgram {
         }
     }
 
+    /**
+     * Thực hiện hành động duy chuyển của con agent.
+     * -Lọc các vị trí đang bị bẩn trong listLocateIsDirty và dựa vào khoảng cách vị trí hiện tại
+     * đên vị trí đang bị bẩn đó mà tìm ra được vị trí bẫn gần nhất vị trí hiện tại đang đứng
+     * @param p
+     * @return
+     */
     public Action move(Percept p) {
+        // Lấy ra vị trí agent đang đúng và chuyển vị trí đó thành thứ tự ô trong đồ thị
         int cellSource = graph.getCellFromLocation(p.getAgentLocation().getRow(), p.getAgentLocation().getCol());
+        // Lấy ra vị trí bẩn gần với vị trí agent đang đứng nhất.
         Location locationDest = getLocationHaveMinDistanceToDirty(p.getAgentLocation());
+        //Chuyển vị trí gần nhất đó thành thứ tự ô trong đồ thị
         int cellDest = graph.getCellFromLocation(locationDest.getRow(), locationDest.getCol());
+        //Lấy đường đi trong đồ thị đã thực hiện thuật toán tìm đường đi ngắn nhất để lấy ra đường đi ngắn nhất từ vị trí hiện tại
+        // đến vị trí bị bẩn gần nhất
         LinkedList<Integer> path = graph.getPathFromProcedureFloyd(cellSource + 1, cellDest + 1);
+        //Lấy ra vị trí tiếp theo sẽ duy chuyển sau khi đã biết đường đi và chuyển nó thành vị trí trong ma trận
         Location moveToLocation = graph.getLocationFromCell(path.get(path.size() - 2));
 
+        //Căn cứ vào vị trí đã tìm ra mà tìm ra hành động hợp lý
         if (p.getAgentLocation().getRow() == moveToLocation.getRow()) {
             if (p.getAgentLocation().getCol() + 1 == moveToLocation.getCol()) {
                 return Environment.MOVE_RIGHT;
@@ -65,6 +87,11 @@ public class AgentProgram {
         return Environment.NO_OP_ACTION;
     }
 
+    /**
+     * Tìm ra vị trí bẩn gần với vị trí hiện tại nhất
+     * @param currentLocation
+     * @return
+     */
     public Location getLocationHaveMinDistanceToDirty(Location currentLocation){
         Location minLocation = listLocateIsDirty.getFirst();
         for(Location lo : listLocateIsDirty){
