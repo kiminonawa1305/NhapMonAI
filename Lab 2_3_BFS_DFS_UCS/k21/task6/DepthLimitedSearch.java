c1b2b19ce06ff43c22bcabd2026eb2a548325c1d
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-09 03:46:50 +0700

    Hoàn thành task 6 Lab2

---
+++ Lab 2_3_BFS_DFS_UCS/k21/task6/DepthLimitedSearch.java
@@ -0,0 +1,42 @@
+package k21.task6;
+
+import java.util.Stack;
+
+public class DepthLimitedSearch implements ISearchAlgo {
+    private int limit;
+
+    public DepthLimitedSearch(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public Node execute(Node root, String goal) {
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+        Node current;
+        a: while(!stack.isEmpty()) {
+            current = stack.peek();
+            if(current.getLabel().equals(goal)) return current;
+
+            if(current.getDepth() < limit){
+                for(Node child : current.getChildrenNodes()) {
+                    if(child.getParent() == null){
+                        stack.add(child);
+                        child.setParent(current);
+                        child.setDepth(current.getDepth() + 1);
+                        continue a;
+                    }
+                }
+            }
+
+
+            stack.pop();
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        return null;
+    }
+}
---
+++ Lab 2_3_BFS_DFS_UCS/k21/task6/TestSearchAlgo.java
@@ -0,0 +1,47 @@
+package k21.task6;
+
+public class TestSearchAlgo {
+    public static void main(String[] args) {
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
+        Node nodeF = new Node("F");
+        Node nodeG = new Node("G");
+        Node nodeH = new Node("H");
+        Node nodeI = new Node("I");
+        Node nodeJ = new Node("J");
+        Node nodeK = new Node("K");
+        Node nodeL = new Node("L");
+        Node nodeM = new Node("M");
+        Node nodeN = new Node("N");
+        Node nodeO = new Node("O");
+        Node nodeP = new Node("P");
+        Node nodeR = new Node("R");
+        Node nodeS = new Node("S");
+
+        nodeA.addEdge(nodeB);
+        nodeA.addEdge(nodeC);
+        nodeA.addEdge(nodeD);
+        nodeB.addEdge(nodeE);
+        nodeB.addEdge(nodeF);
+        nodeC.addEdge(nodeG);
+        nodeD.addEdge(nodeH);
+        nodeE.addEdge(nodeI);
+        nodeF.addEdge(nodeJ);
+        nodeF.addEdge(nodeK);
+        nodeG.addEdge(nodeL);
+        nodeH.addEdge(nodeM);
+        nodeH.addEdge(nodeN);
+        nodeK.addEdge(nodeO);
+        nodeK.addEdge(nodeP);
+        nodeL.addEdge(nodeR);
+        nodeN.addEdge(nodeS);
+
+
+        ISearchAlgo depthLimitedSearch = new DepthLimitedSearch(4);
+        Node result = depthLimitedSearch.execute(nodeA,"R");
+        System.out.println(NodeUtils.printPath(result));
+    }
+}
