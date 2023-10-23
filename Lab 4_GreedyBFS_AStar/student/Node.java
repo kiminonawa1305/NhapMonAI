3773f027f1f96450427405a05c363e4a34e95bcf
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-23 13:54:02 +0700

    Hoàn thành phàn UniformCostSearch.java

--- Lab 4_GreedyBFS_AStar/student/Node.java
+++ Lab 4_GreedyBFS_AStar/student/Node.java
@@ -1,6 +1,7 @@
 package lab4.inform.student;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Node {
---
+++ Lab 4_GreedyBFS_AStar/student/UniformCostSearch.java
@@ -0,0 +1,35 @@
+package lab4.inform.student;
+
+import java.util.PriorityQueue;
+
+public class UniformCostSearch implements IInformedSearchAlgo {
+    @Override
+    public Node execute(Node root, String goal) {
+        PriorityQueue<Node> frontier = new PriorityQueue<Node>((n1, n2) -> {
+            int valueCompare = n1.getLabel().compareTo(n2.getLabel());
+            return valueCompare == 0 ? Double.compare(n1.getG(), n2.getG()) : valueCompare;
+        });
+
+        while(!frontier.isEmpty()){
+            Node current = frontier.poll();
+            if(current.getLabel().equals(goal)){
+                return current;
+            }
+
+            for(Edge edge : current.getChildren()){
+                Node child = edge.getEnd();
+                child.setParent(current);
+                child.setG(current.getG() + edge.getWeight());
+                frontier.add(child);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        Node nodeStart = execute(root, start);
+        nodeStart.setParent(null);
+        return execute(nodeStart, goal);
+    }
+}
