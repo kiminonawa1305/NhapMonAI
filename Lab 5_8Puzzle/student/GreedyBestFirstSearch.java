6f389aef6c72d97275ea1343f1e296a09197f496
Lam Nguyen <kiminonawa1305@gmail.com>
2023-11-04 05:50:26 +0700

    Tạo file GreedyBestFirstSearch.java

---
+++ Lab 5_8Puzzle/student/GreedyBestFirstSearch.java
@@ -0,0 +1,11 @@
+package puzzle_8.student;
+
+import java.util.PriorityQueue;
+
+public class GreedyBestFirstSearch implements IPuzzleAlgo{
+    @Override
+    public Node execute(Puzzle model) throws InterruptedException {
+
+        return null;
+    }
+}
