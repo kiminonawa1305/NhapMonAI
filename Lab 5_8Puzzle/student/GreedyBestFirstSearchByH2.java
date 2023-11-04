f98df5abd155cfbbc190a6e4e36cdcd542aa51e6
Lam Nguyen <kiminonawa1305@gmail.com>
2023-11-04 07:09:58 +0700

    Hiển thị số bước phải di chuyển

--- Lab 5_8Puzzle/student/GreedyBestFirstSearchByH2.java
+++ Lab 5_8Puzzle/student/GreedyBestFirstSearchByH2.java
@@ -7,6 +7,7 @@ public class GreedyBestFirstSearchByH2 implements IPuzzleAlgo {
     @Override
     public Node execute(Puzzle model) {
         System.out.println("=============================Tìm kiếm Greedy Best First theo H2===================================");
+        int step = 0;
         PriorityQueue<Node> frontier = new PriorityQueue<Node>(PuzzleUtils.HeuristicComparatorByH);
         ArrayList<Node> expected = new ArrayList<Node>();
         frontier.add(model.getInitialState());
@@ -16,7 +17,11 @@ public class GreedyBestFirstSearchByH2 implements IPuzzleAlgo {
             Node current = frontier.poll();
             expected.add(current);
             System.out.println(current);
-            if (current.getH() == 0) return current;
+            if (current.getH() == 0) {
+                System.out.println("Số bước phải đi: " + step + " bước");
+                return current;
+            }
+            step++;
 
             for (Node child : model.getSuccessors(current)) {
                 if (!frontier.contains(child) && !expected.contains(child)) {
