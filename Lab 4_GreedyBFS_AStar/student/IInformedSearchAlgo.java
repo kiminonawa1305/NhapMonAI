335cbc69b378c5029ba3f0ed43dcb7099e323c97
Lam Nguyen <kiminonawa1305@gmail.com>
2023-11-17 14:52:59 +0700

    Sửa tên thư mục

--- Lab 4_GreedyBFS_AStar/student/IInformedSearchAlgo.java
+++
@@ -1,8 +0,0 @@
-package lab4.inform.student;
-
-public interface IInformedSearchAlgo {
-	public Node execute(Node root, String goal) throws CloneNotSupportedException;
-
-	public Node execute(Node root, String start, String goal) throws CloneNotSupportedException;
-
-}
